405e8ee550a098dadb9b51b4efe84a7a5c4b08b0
akash <akash.kashyap@sjsu.edu>
2016-12-04 21:02:32 -0800

    user dashboard

--- public/javascripts/profilecontroller.js
+++ public/javascripts/profilecontroller.js
@@ -1,9 +1,10 @@
 
 "use strict"
-  app.controller('profilecontroller',['$scope','$http','$state','$cookies',function ($scope,$http,$state,$cookies){
+  app.controller('profilecontroller',['$scope','$http','$state','$cookies', '$interval',function ($scope, $http, $state, $cookies, $interval){
 
     if($cookies.get('username') ==undefined || $cookies.get('username') =='' || $cookies.get('username') == null){
         //$state.go('admin')
+
 
 
     }
@@ -17,35 +18,177 @@
             $cookies.remove('username');
             $state.go('login')
         }
-    }
 
-      $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-      $scope.series = ['Series A', 'Series B'];
-      $scope.data = [
-          [65, 59, 80, 81, 56, 55, 40],
-          [28, 48, 40, 19, 86, 27, 90]
-      ];
-      $scope.onClick = function (points, evt) {
-          console.log(points, evt);
-      };
-      $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
-      $scope.options = {
-          scales: {
-              yAxes: [
-                  {
-                      id: 'y-axis-1',
-                      type: 'linear',
-                      display: true,
-                      position: 'left'
-                  },
-                  {
-                      id: 'y-axis-2',
-                      type: 'linear',
-                      display: true,
-                      position: 'right'
-                  }
-              ]
-          }
-      };
+        var updatedashboard = function() {
+            console.log('Changing exampleText');
+
+            $scope.user_sensors_count()
+            $scope.user_cluster_count();
+            $scope.user_pending_sensorcount();
+            $scope.user_active_sensorcount();
+            $scope.user_terminated_sensorcount();
+            $scope.user_sensor_per_cluster();
+            $scope.user_sensor_typecount();
+        };
+        
+        $interval(updatedashboard, 5000);
+
+        $scope.user_sensors_count = function(){
+            $http.post(
+                'http://localhost:5000/api/v1/totalsensorsbyaccount',
+                {
+                    username: $cookies.get('username')
+                },
+                { cors: true }
+            )
+                .success(function(data){
+                    if(data.statusCode == 200)
+                    {
+                        $scope.sensors = data.sensors;
+                    }
+                    else
+                    {
+                    }
+                })
+                .error(function(error){
+                    console.log('error')
+                })
+        }
+
+        $scope.user_cluster_count = function(){
+            $http.post(
+                    'http://localhost:5000/api/v1/totalclusterbyaccount',
+                    {
+                        username: $cookies.get('username')
+                    },
+                    { cors: true }
+                )
+                    .success(function(data){
+                        if(data.statusCode == 200)
+                        {
+                            $scope.clusters = data.cluster;
+                        }
+                        else
+                        {
+                        }
+                    })
+                    .error(function(error){
+                        console.log('error')
+                    })
+        }
+
+
+        $scope.user_active_sensorcount = function () {
+
+            $http.post(
+                'http://localhost:5000/api/v1/activesensorsbyaccount',
+                {
+                    username: $cookies.get('username')
+                },
+                { cors: true }
+            )
+                .success(function(data){
+                    if(data.statusCode == 200)
+                    {
+                        $scope.activesensors = data.activesensors;
+                        $scope.activesensorlist = data.result;
+                    }
+                    else
+                    {
+                    }
+                })
+                .error(function(error){
+                    console.log('error')
+                })
+
+        }
+
+        $scope.user_pending_sensorcount = function () {
+
+            $http.post(
+                'http://localhost:5000/api/v1/pendingsensorsbyaccount',
+                {
+                    username: $cookies.get('username')
+                },
+                {cors: true}
+            )
+                .success(function (data) {
+                    if (data.statusCode == 200) {
+                        $scope.pendingsensorscount = data.pendingsensors;
+                        $scope.pendingsensorlist = data.result;
+
+                    }
+                    else {
+                    }
+                })
+                .error(function (error) {
+                    console.log('error')
+                })
+        }
+
+        $scope.user_terminated_sensorcount = function () {
+
+            $http.post(
+                'http://localhost:5000/api/v1/terminatedsensorsbyaccount',
+                {
+                    username: $cookies.get('username')
+                },
+                {cors: true}
+            )
+                .success(function (data) {
+                    if (data.statusCode == 200) {
+                        $scope.terminatedsensorscount = data.terminatedsensors;
+                        $scope.terminatedsensorlist = data.result;
+                    }
+                    else {
+                    }
+                })
+                .error(function (error) {
+                    console.log('error')
+                })
+        }
+
+        $scope.user_sensor_typecount = function () {
+            $http.post(
+                'http://localhost:5000/api/v1/typecountbyaccount',
+                {
+                    username: $cookies.get('username')
+                },
+                {cors: true}
+            )
+                .success(function (data) {
+                    if (data.statusCode == 200) {
+                        $scope.sensorcount = data.count;
+                        $scope.sensortypes = data.types;
+                    }
+                    else {
+                    }
+                })
+                .error(function (error) {
+                    console.log('error')
+                })
+        }
+
+        $scope.user_sensor_per_cluster = function () {
+            $http.post(
+                'http://localhost:5000/api/v1/sensorperclusterbyaccount',
+                {
+                    username: $cookies.get('username')
+                },
+                {cors: true}
+            )
+                .success(function (data) {
+                    if (data.statusCode == 200) {
+                        $scope.clustercount = data.count;
+                        $scope.clusternames = data.clusters;
+                    }
+                    else {
+                    }
+                })
+                .error(function (error) {
+                    console.log('error')
+                })
+        }
+    }
       
   }])
